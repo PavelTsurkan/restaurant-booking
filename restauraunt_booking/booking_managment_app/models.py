3289479a14c86fd6c8ab6e47d974eb42312a61b8
wl_demon_lw <curkanpavel97@gmail.com>
2024-03-02 14:16:34 +0200

    Base project

---
+++ restauraunt_booking/booking_managment_app/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+import db_users_app.models as user_models
+
+
+class Place(models.Model):
+    '''
+    The table's place 
+    '''
+    number = models.IntegerChoices(min=1, max=150) # Table's number
+    is_booked = models.BooleanField(default=False)
+
+
+class Booking(models.Model):
+    '''
+    Model for booking table in restaurant
+    '''
+    user = models.ForeignKey(user_models.User, on_delete=models.CASCADE)    # User who booking table
+    number = models.ForeignKey(Place, on_delete=models.CASCADE)             # Table's number
+    count_of_people = models.IntegerField()                                 # People's number
+    booking_data = models.DateTimeField()                                   # Data & time booking table 
+    valid_until = models.DateTimeField()                                    # Data & time booking table end
+
+    def __str__(self):
+        return f"Користувач: {self.user}, Номер столу:{self.number}, На кількість людей: {self.count_of_people} Заброньовано з: {self.booking_data} ,Діє до: {self.valid_until}"
+    
+
+    class Meta:
+        unique_together = (('user', 'number', 'count_of_people', 'booking_data', 'valid_until'),)
+
