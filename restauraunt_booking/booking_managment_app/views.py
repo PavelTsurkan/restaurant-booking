b1b53e230e8cf247d0369396d60d8fafef6cdcd0
wl_demon_lw <curkanpavel97@gmail.com>
2024-03-10 15:39:54 +0200

    add temlates and some form

--- restauraunt_booking/booking_managment_app/views.py
+++ restauraunt_booking/booking_managment_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import *
 
 
 def index(request):
-    return None
+    return render(request, 'restauraunt_managment_app/index.html')
+
+
+def add_booking(request):
+    places = Place.objects.all()
+
+    if request.method == "POST":
+        print(request.POST)
+    else:
+        print(request.GET)
+
+    return render(request, 'restauraunt_managment_app/add_booking.html', context={"places": places})
--- restauraunt_booking/restauraunt_booking/settings.py
+++ restauraunt_booking/restauraunt_booking/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'booking_managment_app',
-    'bd_users_app',
+    'db_users_app',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
